de7c25626c58c20c3a6fbc2e51349fede2a4bc2a
Ryo inaba <vtryo.work@gmail.com>
2021-12-22 23:21:01 +0900

    initial commit :tada:

---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+)
+
+func timeFormat(time time.Time) {
+	fmt.Println(time.Format("2006/01/02(Mon)"))
+}
+
+func today() {
+	now := time.Now()
+	timeFormat(now)
+}
+func yesterday() {
+	yesterday := time.Now().AddDate(0, 0, -1)
+	timeFormat(yesterday)
+}
+func tomorrow() {
+	tomorrow := time.Now().AddDate(0, 0, 1)
+	timeFormat(tomorrow)
+}
+
+func main() {
+	args := os.Args[1]
+
+	switch args {
+	case "tod":
+		today()
+	case "tom":
+		tomorrow()
+	case "yes":
+		yesterday()
+	}
+}
